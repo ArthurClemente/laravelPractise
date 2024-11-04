a9b820cbdfebbdc61281f5a47b912180e71d1a34
Arthur Clemente <arthurclementte@hotmail.com>
2024-11-04 16:26:03 -0300

    feat: implemented users pagination

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,10 +10,13 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
   public function index() {
-    $user = User::first(); // first return the first element from database
-    // return view('admin/users/index', [ // return the view with the user data
-    //   'user' => $user // the array index is the variable name to be used in the view
+    // $user = User::first(); *first return the first element from database*
+    $users = User::paginate(30); //User::all(); *all or get return all the elements from database* 
+
+    // return view('admin/users/index', [ *return the view with the user data*
+    //   'user' => $user  *the array index is the variable name to be used in the view*
     // ]);
-    return view('admin/users/index', compact('user')); // compact is a helper function that creates an array with the variable name and the variable value
+
+    return view('admin/users/index', compact('users')); // compact is a helper function that creates an array with the variable name and the variable value
   }
 }
