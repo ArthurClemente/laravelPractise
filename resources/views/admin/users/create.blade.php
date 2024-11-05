2506c54509911b61b2ad35fb37ff64906bd2d1d8
Arthur Clemente <arthurclementte@hotmail.com>
2024-11-05 17:43:34 -0300

    feat: new user form created

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Create new user</title>
+</head>
+<body>
+  <form action={{ route('users.createNewUser') }} method="POST">
+    @csrf
+    <input type="text" name="name" placeholder="Name">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit">Create</button>
+  </form>
+</body>
+</html>
