fea4d7f557a569fcaa59378ecb87e29eb303ddbd
Arthur Clemente <arthurclementte@hotmail.com>
2024-11-07 19:09:58 -0300

    feat: show the errors in the page

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,10 +3,20 @@
 @section('title', 'Create new user')
 
 @section('content')
+  <h1>New User</h1>
+
+  @if ($errors->any())
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  @endif
+
   <form action={{ route('users.createNewUser') }} method="POST">
     @csrf
-    <input type="text" name="name" placeholder="Name">
-    <input type="email" name="email" placeholder="Email">
+    <input type="text" name="name" placeholder="Name" {{ old('name') }}>
+    <input type="email" name="email" placeholder="Email"{{ old('email') }}>
     <input type="password" name="password" placeholder="Password">
     <button type="submit">Create</button>
   </form>
